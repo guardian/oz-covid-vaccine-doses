78a2421d8582047ef143a5e1465bb0c9eeb9ee96
josh <josh@thambi.li>
2021-03-05 14:35:26 +1100

    Setup scraper

--- oz_doses_scraper.py
+++ oz_doses_scraper.py
@@ -4,6 +4,14 @@ import os
 
 data_path = os.path.dirname(__file__) + "/data/"
 
-df = pd.read_html("https://www.health.gov.au/initiatives-and-programs/covid-19-vaccines/getting-vaccinated-for-covid-19/how-covid-19-vaccines-will-be-distributed")
+df = pd.read_html("https://www.health.gov.au/initiatives-and-programs/covid-19-vaccines/getting-vaccinated-for-covid-19/how-covid-19-vaccines-will-be-distributed")[0]
 
+columns = ["Jurisdiction", "Description", "Allocation", "Vaccinations", "Vaccination %"]
+
+df = df[3:]
+
+df.columns = columns
 print(df)
+
+with open (f"{data_path}doses.csv", "w") as f:
+    df.to_csv(f, index=False, header=True)
