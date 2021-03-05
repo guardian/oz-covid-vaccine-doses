5837c3a4709783db39501b1bd913eb47f1c37492
josh <josh@thambi.li>
2021-03-05 12:13:27 +1100

    New scraper

--- oz_doses_scraper.py
+++ oz_doses_scraper.py
@@ -1,3 +1,9 @@
 import requests
 import pandas as pd  
+import os
 
+data_path = os.path.dirname(__file__) + "/data/"
+
+df = pd.read_html("https://www.health.gov.au/initiatives-and-programs/covid-19-vaccines/getting-vaccinated-for-covid-19/how-covid-19-vaccines-will-be-distributed")
+
+print(df)
